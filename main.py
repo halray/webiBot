043eede682077c3f4ed867f411b365c74c80ad2b
halray <halrayback@gmail.com>
2023-01-08 20:25:26 +0800

    clear

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 """Readme :  pip install python-telegram-bot --upgrade"""
 from telegram.ext import Updater, CommandHandler, MessageHandler
-import requests
 from config import BOT_TOKEN, API_KEY,IDS_ALLOWED
 
 def main():
     print("====== starting bot program ======")
 
-    updater = Updater(BOT_TOKEN, use_context=True)
+    updater = Updater(BOT_TOKEN, base_url)
+    a = updater.bot.logOut
+    #b = updater.bot.close()
+    print (a)
     dispatcher = updater.dispatcher
 
     # example dog_picture_handler to be demonstrated by teacher
