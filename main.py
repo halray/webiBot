5317b778479a34b7b4a8d9af812dbc1d1ccc5c0c
halray <halrayback@gmail.com>
2023-01-18 16:06:03 +0800

    inline mode test

--- main.py
+++ main.py
@@ -1,15 +1,69 @@
 """Readme :  pip install python-telegram-bot --upgrade"""
-from telegram.ext import Updater, CommandHandler, MessageHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Application, CallbackQueryHandler,ContextTypes
+from telegram import InlineKeyboardButton,InlineKeyboardMarkup,Update
+from telegram.constants import ParseMode
+import requests, requests
+import sys
+sys.path.append('../src/')
 from config import BOT_TOKEN, API_KEY,IDS_ALLOWED
 
-def main():
-    print("====== starting bot program ======")
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
 
-    updater = Updater(BOT_TOKEN, base_url)
-    a = updater.bot.logOut
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Sends a message with three inline buttons attached."""
+    keyboard = [
+        [
+            InlineKeyboardButton("Option 1", callback_data="1"),
+            InlineKeyboardButton("Option 2", callback_data="2"),
+        ],
+        [InlineKeyboardButton("Option 3", callback_data="3")],
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await update.message.reply_text("Please choose:", reply_markup=reply_markup)
+
+
+async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Parses the CallbackQuery and updates the message text."""
+    query = update.callback_query
+
+    # CallbackQueries need to be answered, even if no notification to the user is needed
+    # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
+    await query.answer()
+
+    await query.edit_message_text(text=f"Selected option: {query.data}")
+
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Displays info on how to use the bot."""
+    await update.message.reply_text("Use /start to test this bot.")
+
+
+def main() -> None:
+    """Run the bot."""
+    # Create the Application and pass it your bot's token.
+    application = Application.builder().token("TOKEN").build()
+
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CallbackQueryHandler(button))
+    application.add_handler(CommandHandler("help", help_command))
+
+    # Run the bot until the user presses Ctrl-C
+    application.run_polling()
+
+#def main():
+#   print("====== starting bot program ======")
+
+ #   updater = Updater(BOT_TOKEN, base_url)
+  #  a = updater.bot.logOut
     #b = updater.bot.close()
-    print (a)
-    dispatcher = updater.dispatcher
+   # print (a)
+    #dispatcher = updater.dispatcher
 
     # example dog_picture_handler to be demonstrated by teacher
     #dispatcher.add_handler(CommandHandler("dog", dog_picture_handler))
@@ -17,8 +71,8 @@ def main():
     # movie_handler to be filled in by student
     #dispatcher.add_handler(CommandHandler("movie", movie_handler))
 
-    updater.start_polling()
-    updater.idle()
+    #updater.start_polling()
+    #updater.idle()
 
 
 if __name__ == "__main__":
